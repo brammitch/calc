9547de90b725cfe5fa61e7a7dd013180f739646b
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2022-03-24 18:41:59 -0400

    done; but no extra credit yet

--- script.js
+++ script.js
@@ -50,23 +50,35 @@ const operators = {
  */
 
 /**
+ * @typedef { '+' | '-' | '*' | '/' | '=' } OperatorChar
+ */
+
+/**
  *
- * @param {Operator} operator
+ * @param {Operator} o
  * @param {number} a
  * @param {number} b
  * @returns number
  */
-function operate(operator, a, b) {
-  return operators[operator](a, b);
+function operate(o, a, b) {
+  return operators[o](a, b);
 }
 
 let display = "0";
-let value = 0;
+let operationComplete = false;
+let operator = null;
+let storedInput = "";
+let value = null;
+
 const displayEl = document.querySelector(".display");
 displayEl.innerHTML = display;
 
+const storedInputEl = document.querySelector(".stored-input");
+storedInputEl.innerHTML = storedInput;
+
 function updateDisplay(num) {
-  if (display === "0") {
+  if (display === "0" || operationComplete) {
+    operationComplete = false;
     display = `${num}`;
   } else {
     display += num;
@@ -114,4 +126,85 @@ eightEl.addEventListener("click", () => {
 const nineEl = document.getElementById("nine");
 nineEl.addEventListener("click", () => {
   updateDisplay(9);
+});
+
+function clearDisplay() {
+  value = null;
+  display = "0";
+  storedInput = "";
+  displayEl.innerHTML = display;
+  storedInputEl.innerHTML = storedInput;
+}
+
+const clearEl = document.getElementById("clear");
+clearEl.addEventListener("click", () => {
+  clearDisplay();
+});
+
+const deleteEl = document.getElementById("delete");
+deleteEl.addEventListener("click", () => {
+  display = display.slice(0, -1);
+  displayEl.innerHTML = display;
+});
+
+// const decimalEl = document.getElementById("decimal");
+/**
+ *
+ * @param {Operator} o
+ * @param {OperatorChar} oc
+ * @returns void
+ */
+function handleOperator(o, oc) {
+  operator = o;
+  const displayValue = parseInt(display, 10);
+  if (value === null) {
+    value = displayValue;
+    operationComplete = true;
+  } else {
+    if (o === "divide" && displayValue === 0) {
+      clearDisplay();
+      display = "Cannot divide by zero!";
+      displayEl.innerHTML = display;
+      operationComplete = true;
+      return;
+    } else {
+      value = operate(operator, value, displayValue);
+      operationComplete = true;
+    }
+  }
+  display = `${value}`;
+  displayEl.innerHTML = display;
+  if (oc === "=") {
+    operationComplete = true;
+    storedInput = "";
+    value = null;
+  } else {
+    storedInput = value + ` ${oc} `;
+  }
+  storedInputEl.innerHTML = storedInput;
+}
+
+const addEl = document.getElementById("add");
+addEl.addEventListener("click", () => {
+  handleOperator("add", "+");
+});
+
+const subtractEl = document.getElementById("subtract");
+subtractEl.addEventListener("click", () => {
+  handleOperator("subtract", "-");
+});
+
+const multiplyEl = document.getElementById("multiply");
+multiplyEl.addEventListener("click", () => {
+  handleOperator("multiply", "*");
+});
+
+const divideEl = document.getElementById("divide");
+divideEl.addEventListener("click", () => {
+  handleOperator("divide", "/");
+});
+
+const equalsEl = document.getElementById("equals");
+equalsEl.addEventListener("click", () => {
+  handleOperator(operator, "=");
 });
